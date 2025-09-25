118f2f44ae21155add2d448a489118266a63a296
Raj Janrao <rajjanrao@Rajs-MacBook-Air.local>
2025-09-25 23:23:19 +0530

    Logo Component has been added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Button from "../Button/Button";
+import Logo from "../Logo/Logo";
 import Search from "../Search/Search";
-import styles from './Navbar.module.css'
-import LogoImage from "../../assets/Logo.png"
+import styles from "./Navbar.module.css";
 
-
-export default function Navbar(){
-    return(
-        <>
-            <nav className={styles.navbar}>
-                <div className={styles.logoDiv}><img  src={LogoImage} alt="logo" width={67}/></div>
-                 <Search search={"Search a song of your choice"}/>
-                <Button children="Give Feedback"/>
-            </nav>
-        </>
-    )
+function Navbar({ searchData }) {
+  return (
+    <nav className={styles.navbar}>
+      <Link to="/">
+        <Logo />
+      </Link>
+      <Search
+        placeholder="Search a song of your choice"
+        searchData={searchData}
+      />
+      <Button>Give Feedback</Button>
+    </nav>
+  );
 }
+
+export default Navbar;
